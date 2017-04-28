cc6c0af895671bacabf4f1f146eecea86b596bd6
Ignacio Saporiti <saporiti.ignacio@gmail.com>
2017-04-28 01:11:25 -0300

    FIx.

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const async = require('async');
 const dbConnection = require('./db_connection');
 const Twitter = require('twitter');
 const twitterClient = new Twitter({
-        consumer_key: process.env.twitter_consumer_key,
-        consumer_secret: process.env.twitter_consumer_secret,
-        access_token_key: process.env.twitter_access_token_key,
-        access_token_secret: process.env.twitter_access_token_secret
-    });
+	consumer_key: process.env.twitter_consumer_key,
+	consumer_secret: process.env.twitter_consumer_secret,
+	access_token_key: process.env.twitter_access_token_key,
+	access_token_secret: process.env.twitter_access_token_secret
+});
 const params = { track: process.argv[2] };
+const tweetsToProcess = 10;
 
 dbConnection(process.env.mongodb_url)
 	.then((db) => {
@@ -22,47 +23,45 @@ dbConnection(process.env.mongodb_url)
 		let document;
 		let count = 0;
 		stream.on('data', (tweet) => {
-			if (!tweet.text.startsWith('RT')) {
-				async.waterfall([
-					(callback) => {
-						const document = {
-							text: tweet.text,
-							user: {
-								name: tweet.user.name,
-								screen_name: tweet.user.screen_name,
-								description: tweet.user.description,
-								lang: tweet.user.lang,
-								location: tweet.user.location
-							},
-							created_at: tweet.created_at,
-							timestamp_ms: tweet.timestamp_ms
-						};
-						callback(null, document);
-					},
-					(document, callback) => {
-						async.series({
-							insert: (callback) => {
-								db.collection('tweets').insertOne(document, (error, result) => {
-									callback();
-								});
-							},
-							count: (callback) => {
-								db.collection('tweets').count((err, dbCount) => {
-									console.log(`Tweets inserted in DB: ${dbCount}.`);
-									callback(null, dbCount);
-								});
-							}
-						});
-						callback();
-					}
-				], (error, results) => {
-					if (++count > config.tweetsToProcess) {
-						console.log(`Process finished: Tweets inserted on this run: ${config.tweetsToProcess}.\nKeyword/s: "${process.argv[2]}".`);
-						process.exit();
-					}
-				});
+			async.waterfall([
+				(callback) => {
+					const document = {
+						text: tweet.text,
+						user: {
+							name: tweet.user.name,
+							screen_name: tweet.user.screen_name,
+							description: tweet.user.description,
+							lang: tweet.user.lang,
+							location: tweet.user.location
+						},
+						created_at: tweet.created_at,
+						timestamp_ms: tweet.timestamp_ms
+					};
+					callback(null, document);
+				},
+				(document, callback) => {
+					async.series({
+						insert: (callback) => {
+							db.collection('tweets').insertOne(document, (error, result) => {
+								callback();
+							});
+						},
+						count: (callback) => {
+							db.collection('tweets').count((err, dbCount) => {
+								console.log(`Tweets inserted in DB: ${dbCount}.`);
+								callback(null, dbCount);
+							});
+						}
+					});
+					callback();
+				}
+			], (error, results) => {
+				if (++count > tweetsToProcess) {
+					console.log(`Process finished: Tweets inserted on this run: ${tweetsToProcess}.\nKeyword/s: "${process.argv[2]}".`);
+					process.exit();
+				}
+			});
 
-			}
 		});
 		stream.on("error", (error) => {
 			throw error;
